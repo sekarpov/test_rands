8156b96901c6d5eb42edfe20ed5dd5d534803c1d
sekarpov <sergo8ck@gmail.com>
2021-03-06 22:34:55 +0300

    dependencies, container bootstrap

--- public/index.php
+++ public/index.php
@@ -2,21 +2,15 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new DI\ContainerBuilder();
-
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ],
-]);
-
-$container = $builder->build();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
 $app = AppFactory::createFromContainer($container);
 
